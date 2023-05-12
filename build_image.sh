7fc70b30b073968166a00754bb8fa884d40d2a6d
Rachael Lau <laurachael98@gmail.com>
2023-05-12 09:40:25 +0000

    Fix subdomain

--- build_image.sh
+++ build_image.sh
@@ -1,3 +1,3 @@
 docker build -t wsus-privoxy .
-docker tag wsus-privoxy cmrachael/wsus-privoxy:stable
-docker push cmrachael/wsus-privoxy:stable
+docker tag wsus-privoxy cmrachael/wsus-privoxy:latest
+docker push cmrachael/wsus-privoxy:latest
